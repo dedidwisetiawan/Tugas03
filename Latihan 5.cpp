c6de63da7b7a5837abe0e6b8289deb241767f7d0
dedidwi <32447378+dedidwisetiawan@users.noreply.github.com>
2017-11-08 16:32:27 +0800

    Salah

---
+++ Latihan 5.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int tb, bb, hasil1, hasil2;
+	cout<<"Menghitung Badan Ideal"<<endl;
+	cout<<"Masukan Tinggi Badan = ";cin>>tb;
+	cout<<"Masukan Berat Badan  = ";cin>>bb;
+	
+	if(bb>tb/2.5){
+		cout<<"Anda Termasuk UNDERWEIGHT";
+	}
+	else if(tb/2.3<<bb){
+		cout<<"Anda Termasuk Overweight";
+	}
+	else if(tb/2.5<=bb<=tb/2.3){
+		cout<<"Anda Termasuk Normal";
+	}
+	
+}
